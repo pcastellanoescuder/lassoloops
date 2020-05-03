e7bf28ca75b7e83fb858b37d609a37b51b6fbad6
pcastellanoescuder <pol@MacBook-Pro-de-Pol.local>
2020-05-03 15:12:54 +0200

    add mtcars data

--- R/data.R
+++ R/data.R
@@ -19,3 +19,25 @@
 #' }
 #' @references Loprinzi CL. Laurie JA. Wieand HS. Krook JE. Novotny PJ. Kugler JW. Bartel J. Law M. Bateman M. Klatt NE. et al. Prospective evaluation of prognostic variables from patient-completed questionnaires. North Central Cancer Treatment Group. Journal of Clinical Oncology. 12(3):601-7, 1994.
 "survival_cancer"
+
+#' mtcars dataset from `datasets` package (Motor Trend Car Road Tests)
+#'
+#' The data was extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973–74 models).
+#'
+#' @format A data frame with 32 observations on 11 (numeric) variables.
+#' \describe{
+#'   \item{mpg}{Miles/(US) gallon}
+#'   \item{cyl}{Number of cylinders}
+#'   \item{disp}{Displacement (cu.in.)}
+#'   \item{hp}{Gross horsepower}
+#'   \item{drat}{Rear axle ratio}
+#'   \item{wt}{Weight (1000 lbs)}
+#'   \item{qsec}{1/4 mile time}
+#'   \item{vs}{Engine (0 = V-shaped, 1 = straight)}
+#'   \item{am}{Transmission (0 = automatic, 1 = manual)}
+#'   \item{gear}{Number of forward gears}
+#'   \item{carb}{Number of carburetors}
+#' }
+#' @source Henderson and Velleman (1981), Building multiple regression models interactively. Biometrics, 37, 391–411.
+"mtcars"
+
