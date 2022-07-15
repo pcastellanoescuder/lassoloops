a3bad6755c8c5327ab880f965ccef05c2d06a937
Pol Castellano Escuder <polcaes@gmail.com>
2022-07-15 12:26:04 -0400

    test parameter in blasso

--- R/blasso.R
+++ R/blasso.R
@@ -11,6 +11,7 @@
 #' @param nfolds Number of folds - default is 10. Although nfolds can be as large as the sample size (leave-one-out CV), it is not recommended for large datasets. Smallest value allowable is nfolds=3.
 #' @param offset A vector of length `nobs` that is included in the linear predictor. See `?glmnet::glmnet()`
 #' @param family Response type. Quantitative for family = "gaussian" or family = "poisson" (non-negative counts).
+#' @param ntest Numeric indicating the percentage of observations that will be used as test set. Default is NULL (no test set).
 #' @param seed `set.seed()` that will be used.
 #' @param ncores Number of cores. Each loop will run in one core using the `foreach` package.
 #'
@@ -32,6 +33,7 @@ blasso <- function(x,
                    nfolds = 10,
                    offset = NULL,
                    family = "gaussian",
+                   ntest = NULL,
                    seed = 987654321,
                    ncores = 2){
 
@@ -53,40 +55,42 @@ blasso <- function(x,
   res <- foreach::foreach(i = 1:loops) %dopar% {
 
     ## BOOTSTRAP
-
     if(bootstrap){
-
       idx <- sample(1:n, replace = T)
 
       new_matrix <- cbind(y, x)
       new_matrix <- new_matrix[idx ,]
-
-    } else{
-
+    } else {
       new_matrix <- cbind(y, x)
-
     }
 
-    ## TEST
+    if(!is.null(ntest)){
+      ## TEST
+      idx_test <- sample(1:n, 0.2*n, replace = FALSE)
 
-    idx_test <- sample(1:n, 0.2*n, replace = FALSE)
+      test <- new_matrix[idx_test ,]
+      test_x <- test[,-1]
+      test_y <- test[,1]
 
-    test <- new_matrix[idx_test ,]
-    test_x <- test[,-1]
-    test_y <- test[,1]
+      ## TRAIN
+      train <- new_matrix[-idx_test ,]
+      train_x <- train[,-1]
+      train_y <- train[,1]
 
-    ## TRAIN
+      ## LASSO
+      suppressWarnings(
+        cv_fit <- glmnet::cv.glmnet(data.matrix(train_x), as.matrix(train_y), alpha = alpha,
+                                    family = family, nfolds = nfolds,
+                                    offset = offset)
+      )
 
-    train <- new_matrix[-idx_test ,]
-    train_x <- train[,-1]
-    train_y <- train[,1]
-
-    ## LASSO
-    suppressWarnings(
-      cv_fit <- glmnet::cv.glmnet(data.matrix(train_x), as.matrix(train_y), alpha = alpha,
-                                  family = family, nfolds = nfolds,
-                                  offset = offset)
-    )
+    } else {
+      suppressWarnings(
+        cv_fit <- glmnet::cv.glmnet(data.matrix(new_matrix[,-1]), as.matrix(new_matrix[,1]), alpha = alpha,
+                                    family = family, nfolds = nfolds,
+                                    offset = offset)
+      )
+    }
 
     tmp_coeffs <- coef(cv_fit, s = "lambda.min")
     final_coef <- data.frame(name = tmp_coeffs@Dimnames[[1]][tmp_coeffs@i + 1], coefficient = tmp_coeffs@x)
@@ -98,7 +102,12 @@ blasso <- function(x,
       lasso_pred <- predict(cv_fit, s = cv_fit$lambda.min, newx = data.matrix(test_x))
     }
 
-    mse <- mean((test_y - lasso_pred)^2)
+    if(!is.null(ntest)){
+      mse <- mean((test_y - lasso_pred)^2)
+    } else {
+      mse <- NULL
+    }
+
     res[[i]] <- list(coeffs = final_coef, mse = mse, model = cv_fit)
 
   }
